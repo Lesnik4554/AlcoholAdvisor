d9cb33fd056b2199c2b0f1118338f3fa68fb8a22
Kirill Slepenkov <kirill2595@gmail.com>
2019-03-21 15:07:44 +0500

    Initial commit

---
+++ app/src/main/java/com/kirill/slepenkov/alcoholadvisor/PoliticsMainActivity.java
@@ -0,0 +1,75 @@
+package com.kirill.slepenkov.alcoholadvisor;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+
+public class PoliticsMainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.politics_activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.low_alcohol) {
+            // Handle the camera action
+        } else if (id == R.id.medium_alcohol) {
+
+        } else if (id == R.id.strong_alcohol) {
+
+        } else if (id == R.id.politics) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //Метод клика по поделиться
+    public void onClickShare2(MenuItem item) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Я использую крутое приложение, чтобы " +
+                                                      "читать Алкогольный советник. Скачай и ты:");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent,"Поделиться"));
+    }
+}
